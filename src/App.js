b6016d34aa009b8a97994083376b6d236b1bc936
Zachary Taylor <zpanther8920@yahoo.com>
2021-09-26 22:57:42 -0400

    created the popup box for the pin and added css to it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
-import { Room } from "@material-ui/icons";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
+import "./app.css"
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -25,9 +26,35 @@ function App() {
         >
          <Room style={{fontSize:viewport.zoom *6, color:"cornflowerblue"}}/>
         </Marker>
+        <Popup
+          latitude={40.748817}
+          longitude={-73.985428}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left" >
+          <div className ="card">
+            <label>Place</label>
+            <h4 className="Place">Empire State Building</h4>
+            <label>Review</label>
+            <p className="desc">Amazing, it was huge!</p>
+            <label>Rating</label>
+            <div className="stars">
+              <Star />
+              <Star />
+              <Star />
+              <Star />
+              <Star />
+            </div>
+            <label>Information</label>
+            <span className="username">Created by <b>Zach</b></span>
+            <span className="date">1 hour ago</span>
+          </div>
+        </Popup>
       </ReactMapGL>
     </div>
   );
 }
 
 export default App;
+
+//58:00
