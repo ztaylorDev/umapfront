c6fbf25a98885efdff7be6aee376feac47935caf
Zachary Taylor <zpanther8920@yahoo.com>
2021-09-28 22:13:31 -0400

    created a double click feature with a pop up box to add comments and places

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { Room, Star } from "@material-ui/icons";
-import "./app.css"
+import "./app.css";
+import axios from "axios";
+import { format } from "timeago.js";
 
 function App() {
+  const currentUser = "zach";
+  const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [newPlace, setNewPlace] = useState(null);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -11,45 +17,121 @@ function App() {
     longitude: -100.0,
     zoom: 4,
   });
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const allPins = await axios.get("/pins");
+        setPins(allPins.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
+
+  const handleMarkerClick = (id,lat,long) => {
+    setCurrentPlaceId(id);
+    setViewport({...viewport, latitude: lat, longitude: long});
+  };
+
+  const handleAddClick = (e) => {
+    const [long, lat] = e.lngLat;
+    setNewPlace({
+      lat,
+      long,
+    });
+  };
+
   return (
     <div className="App">
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        onDblClick={handleAddClick}
+        //transitionDuration = "150"
       >
-        <Marker
-          latitude={40.748817}
-          longitude={-73.985428}
-          offsetLeft={-20}
-          offsetTop={-10}
-        >
-         <Room style={{fontSize:viewport.zoom *6, color:"cornflowerblue"}}/>
-        </Marker>
-        <Popup
-          latitude={40.748817}
-          longitude={-73.985428}
+        {pins.map((p) => (
+          <>
+            <Marker
+              latitude={40.748817}
+              longitude={-73.985428}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <Room
+                style={{
+                  fontSize: viewport.zoom * 6,
+                  color:
+                    p.username === currentUser ? "tomato" : "cornflowerblue",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleMarkerClick(p._id,p.lat,p.long)}
+              />
+            </Marker>
+            {p._id === currentPlaceId && (
+              <Popup
+                latitude={p.lat}
+                longitude={p.long}
+                closeButton={true}
+                closeOnClick={false}
+                anchor="left"
+                onClose={() => setCurrentPlaceId(null)}
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="Place">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="desc">{p.desc}</p>
+                  <label>Rating</label>
+                  <div className="stars">
+                    <Star />
+                    <Star />
+                    <Star />
+                    <Star />
+                    <Star />
+                  </div>
+                  <label>Information</label>
+                  <span className="username">
+                    Created by <b>{p.username}</b>
+                  </span>
+                  <span className="date">{format(p.createdAt)}</span>
+                </div>
+              </Popup>
+            )}
+          </>
+        ))}
+        {newPlace && (
+
+          <Popup
+          latitude={newPlace.lat}
+          longitude={newPlace.long}
           closeButton={true}
           closeOnClick={false}
-          anchor="left" >
-          <div className ="card">
-            <label>Place</label>
-            <h4 className="Place">Empire State Building</h4>
-            <label>Review</label>
-            <p className="desc">Amazing, it was huge!</p>
-            <label>Rating</label>
-            <div className="stars">
-              <Star />
-              <Star />
-              <Star />
-              <Star />
-              <Star />
-            </div>
-            <label>Information</label>
-            <span className="username">Created by <b>Zach</b></span>
-            <span className="date">1 hour ago</span>
-          </div>
-        </Popup>
+          anchor="left"
+          onClose={() => setNewPlace(null)}
+
+          >
+           <div>
+              <form>
+                <label>Title</label>
+                <input placeholder="Enter a title" />
+                <label>Review</label>
+                <textarea placeholder="Leave a review" />
+                <label>Rating</label>
+                <select>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <button className="submitButton" type="submit">Drop a Pin</button>
+              </form>  
+           </div> 
+          </Popup>
+        )}
       </ReactMapGL>
     </div>
   );
@@ -57,4 +139,4 @@ function App() {
 
 export default App;
 
-//58:00
+//1:04:55
